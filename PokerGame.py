49c12a2d0cfb756ac3cbf484af125744e587ee33
Rahul <rahul.kalaiselvan@gmail.com>
2019-10-05 19:51:57 -0600

    Update PokerGame.py

--- PokerGame.py
+++ PokerGame.py
@@ -1,11 +1,27 @@
 import unittest
 
 class pokerGame:
-	def calculateHand(self):
+	# [A, K, Q, 10, 9, 8, 6, 5, 4, 3, 2]
+	# [S, H, D, C]
+	def findWinner(self, score1, score2):
+
+
+	def findHighest(self, hand1, hand2):
+		value = {'2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7, '9':6, '10':7, 'Q':8, 'K':9, 'A':10}
+
+
+	def findSpecial(self, hand):
+		highestCard = findHighest(hand)
 		return 0
 	
 	def gameResult(self, player_one, player_two):
-		return 0
+		scoreOfPlayer1 = findSpecial(player_one)
+		scoreOfPlayer2 = findSpecial(player_two)
+		
+		if scoreOfPlayer1 == 0 and scoreOfPlayer2 == 0:
+			result = findHighest(player_one, player_two)
+		else:
+			result = findWinner(scoreOfPlayer1, scoreOfPlayer2)
 
 
 def getWinner(player_one, player_two):
@@ -14,10 +30,10 @@ def getWinner(player_one, player_two):
 	return 0
 
 class OutcomesTest(unittest.TestCase):
-	def test1(self):
-		hand1 = [1,2,3]
-		hand2 = [3,4,5]
-		self.assertEqual(getWinner(hand1, hand2), 0)
+	def equalHands(self):
+		hand1 = [S6, C2, D9, D4, HK]
+		hand2 = [SA, C2, D3, D6, H10]
+		self.assertEqual(getWinner(hand1, hand2), 2)
 
 if __name__ == '__main__':
     unittest.main()
