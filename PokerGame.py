1c520dde7ff4ec61604cc33ecbfa592a36e63f2e
Rahul <rahul.kalaiselvan@gmail.com>
2019-10-06 17:42:31 -0600

    Update PokerGame.py

--- PokerGame.py
+++ PokerGame.py
@@ -1,6 +1,6 @@
 import unittest
 
-class pokerGame:
+class PokerGame:
 	# [A, K, Q, 10, 9, 8, 6, 5, 4, 3, 2]
 	# [S, H, D, C]
 	valueOfRank = {'2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7, '9':8, '10':9, 'J':10, 'Q':11, 'K':12, 'A':13}
@@ -42,6 +42,16 @@ class pokerGame:
 		else:
 			return 0
 
+	def findMaxValue(self, list1, list2, n):
+		for i in list1:
+			if i[1] == n:
+				val1 = i[0]
+		for i in list2:
+			if i[1] == n:
+				val2 = i[0]
+
+		return val1, val2
+
 
 	def findPairs(self, hand):
 		outList = []
@@ -56,10 +66,10 @@ class pokerGame:
 				outList.append([li[i], pairs])
 		return outList
 
+
 	def findPairs2(self, hand):
 		outList = []
 		li = hand
-		print('FP',li)
 		for i in range(len(li)):
 			pairs = 0
 			for j in range(len(li)):
@@ -71,7 +81,6 @@ class pokerGame:
 		return outList
 
 
-
 	def find_maximumPair(self, listOfValues):
 		li = []
 		li2 = []
@@ -81,32 +90,78 @@ class pokerGame:
 		return max(li)
 
 
+	def compare_HighestNotEqual(self, list1, list2):
+		i = 0
+		while len(list1)>0:
+			if list1[i] == list2[i]:
+				list1.remove(list1[i])
+				list2.remove(list2[i])
+			else:
+				return self.compare(list1[i], list2[i])
+
+
+	def compare_SinglePair(self, hand1, hand2):
+		h1_list = []
+		h2_list = []
+
+		for i in hand1:
+			if i[1] == 1:
+				h1_list.append(i[0])
+
+		for i in hand2:
+			if i[1] == 1:
+				h2_list.append(i[0])
+
+		h1_list.sort(reverse = True)
+		h2_list.sort(reverse = True)
+		return self.compare_HighestNotEqual(h1_list, h2_list)
+
 
 	def compareHands(self, hand1, hand2):
-		print(hand1, hand2)
 		h1 = self.findPairs(hand1)
 		h2 = self.findPairs(hand2)
 
 		maxVal = self.find_maximumPair(h1)
-		print(maxVal)
 
 		if maxVal == 4:
-			for i in h1:
-				if i[1] == 1:
-					val1 = i[0]
-			for i in h2:
-				if i[1] == 1:
-					val2 = i[0]
+			val1, val2 = self.findMaxValue(h1, h2, 1)
 
 		elif maxVal == 3:
-			print('here')
+			val1, val2 = self.findMaxValue(h1, h2, 2)
+
+		elif maxVal == 2:
+			val_pair_1 = []
+			val_pair_2 = []
 			for i in h1:
 				if i[1] == 2:
-					val1 = i[0]
+					val_pair_1.append(i[0])
+			val_pair_1.sort()
+
 			for i in h2:
 				if i[1] == 2:
-					val2 = i[0]
+					val_pair_2.append(i[0])
+			val_pair_2.sort()
 
+			if len(val_pair_1) > 1:
+				if max(val_pair_1) != max(val_pair_2):
+					val1 = max(val_pair_1)
+					val2 = max(val_pair_2)
+
+				elif val_pair_1[len(val_pair_1)-2] != val_pair_2[len(val_pair_2)-2]:
+					val1 = val_pair_1[len(val_pair_1)-2]
+					val2 = val_pair_2[len(val_pair_2)-2]
+
+				else:
+					val_result = self.compare_SinglePair(h1, h2)
+					return val_result
+
+			elif len(val_pair_1) == 1:
+				if val_pair_1[0] == val_pair_2[0]:
+					val_result = self.compare_SinglePair(h1, h2)
+					return val_result
+
+				else:
+					return compare(val_pair_1[0], val_pair_2[0])
 		return self.compare(val1, val2)
 
 
@@ -121,24 +176,29 @@ class pokerGame:
 					return 'fullHouse'
 				else:
 					return 'threeOfaKind'
-			elif i[1] == 2:
-				return 'twoPairs'
-
+			elif i[1] == 2: 
+				if len(resultList) == 3:
+					return 'twoPairs'
+				else:
+					return 'Pair'
 		return flag
 
 
 	def check_StraightFlush(self, hand):
 		rankl = self.splitRank(hand)
-		rankl.sort()
+		rankl.sort(reverse = True)
 		flag = 0
 		temp = rankl[0]
 		for i in range(len(rankl)):
 			if i != 0:
-				if rankl[i] == temp-1:
-					temp = rankl[i]
-				else:
-					if rankl[i] != 13:
+				if rankl[i] != temp-1:
+					if rankl[i] == 4 and temp == 13:
+						temp = rankl[i]
+					else:
 						flag = 1
+
+				else:
+					temp = rankl[i]
 
 		if flag == 0:
 			return 1
@@ -192,12 +252,7 @@ class pokerGame:
 			h1 = self.splitRank(hand1)
 			h2 = self.splitRank(hand2)
 
-			if h1[0]>h2[0]:
-				return 1
-			elif h2[0]>h1[0]:
-				return 2
-			else:
-				return 0
+			return self.compare(h1[0], h2[0])
 
 		elif combination == 2:
 			result = self.compareHands(hand1, hand2)
@@ -207,11 +262,21 @@ class pokerGame:
 			result = self.compareHands(hand1, hand2)
 			return result
 
+		elif combination == 7:
+			result = self.compareHands(hand1, hand2)
+			return result
+
+		elif combination == 8:
+			result = self.compareHands(hand1, hand2)
+			return result
+
 
 	def findCombination(self, hand):
 		if self.check_FullSuit(hand) == 1:
 			if self.check_StraightFlush(hand) == 1:
 				return 1
+			else:
+				return 4
 	
 		if self.check_Pairs(hand) == 'fourOfaKind':
 			return 2
@@ -219,14 +284,17 @@ class pokerGame:
 		elif self.check_Pairs(hand) == 'fullHouse':
 			return 3
 
-		elif self.check_Pairs(hand) == 'threeOfaKind':
-			return 4
-
 		elif self.check_StraightFlush(hand) == 1:
 			return 5
 
-		elif self.check_Pairs(hand) == 'twoPairs':
+		elif self.check_Pairs(hand) == 'threeOfaKind':
 			return 6
+
+		elif self.check_Pairs(hand) == 'twoPairs':
+			return 7
+
+		elif self.check_Pairs(hand) == 'Pair':
+			return 8
 
 		#highestCard = findHighest(hand)
 		return 10
@@ -244,29 +312,3 @@ class pokerGame:
 			result = self.findWinner(scoreOfPlayer1, scoreOfPlayer2)
 
 		return result
-
-
-def getWinner(player_one, player_two):
-	game = pokerGame()
-	gameResult = game.gameResult(player_one, player_two)
-	print(gameResult)
-	return gameResult
-
-
-# class OutcomesTest(unittest.TestCase):
-# 	def equalHands(self):
-# 		hand1 = ['S6', 'C2', 'D9', 'D4', 'HK']
-# 		hand2 = ['SJ', 'C2', 'D3', 'D6', 'H10']
-# 		self.assertEqual(getWinner(hand1, hand2), 2)
-
-
-# if __name__ == '__main__':
-#     unittest.main()
-
-#getWinner(['S-6', 'C-2', 'D-9', 'D-4', 'H-K'], ['S-A', 'C-2', 'D-3', 'D-6', 'H-10'])
-#getWinner(['S-A', 'S-K', 'S-Q', 'S-J', 'S-10'], ['S-A', 'C-2', 'D-3', 'D-6', 'H-10'])
-#getWinner(['S-A', 'S-K', 'S-Q', 'S-J', 'S-10'], ['S-5', 'S-4', 'S-3', 'S-2', 'S-A'])
-#getWinner(['S-A', 'C-A', 'D-A', 'C-A', 'S-10'], ['S-5', 'S-4', 'S-3', 'S-2', 'S-A'])
-#getWinner(['S-A', 'C-A', 'D-2', 'C-5', 'S-8'], ['S-5', 'S-5', 'S-5', 'S-2', 'S-10'])
-#getWinner(['S-A', 'C-A', 'D-A', 'C-A', 'S-6'], ['S-A', 'S-A', 'S-A', 'D-A', 'S-10'])
-getWinner(['S-A', 'C-A', 'D-A', 'C-3', 'S-3'], ['S-A', 'S-A', 'S-A', 'D-5', 'S-5'])
